1be2ba464133374595199e85e1a2a2de3fc9e9e2
Henrique Galo <henriqueg@me.com>
2018-12-22 02:03:31 -0200

    Initial Commit

---
+++ HGGIF/HGGIF.h
@@ -0,0 +1,19 @@
+//
+//  HGGIF.h
+//  HGGIF
+//
+//  Created by Henrique Galo on 22/12/18.
+//  Copyright © 2018 Henrique Galo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HGGIF.
+FOUNDATION_EXPORT double HGGIFVersionNumber;
+
+//! Project version string for HGGIF.
+FOUNDATION_EXPORT const unsigned char HGGIFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HGGIF/PublicHeader.h>
+
+
